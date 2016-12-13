74161effaa47d3fc7804ac0bde9baa833f4980ab
wangyouzhan <wangyouzhan1987@163.com>
2016-12-13 12:23:30 +0800

    add cpp jni

---
+++ app/src/main/java/com/imooc/wangyouzhan/chatclient/Activity/NDKActivity.java
@@ -0,0 +1,54 @@
+package com.imooc.wangyouzhan.chatclient.Activity;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.imooc.wangyouzhan.chatclient.R;
+
+public class NDKActivity extends AppCompatActivity {
+
+
+    private TextView tv_show;
+    private Button btn_binder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ndk);
+
+        tv_show = (TextView) findViewById(R.id.tv_show);
+        btn_binder = (Button) findViewById(R.id.btn_binder);
+        btn_binder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                tv_show.setText(hello() + getFromJNI() + sayHello());
+                Toast.makeText(getApplicationContext(), getFromJNI(), Toast.LENGTH_SHORT).show();
+                Log.w("dd", Environment.getExternalStorageDirectory().getAbsolutePath());
+                String flag = updateFile(Environment.getExternalStorageDirectory().getAbsolutePath() + "/body.txt");
+
+
+            }
+        });
+    }
+
+
+
+    public native String hello();
+    public native String getFromJNI();
+    public native String sayHello();
+    public static native String updateFile(String file);
+
+    static {
+        System.loadLibrary("JniDemo");
+    }
+
+
+
+
+}
---
+++ app/src/main/jni/JniDemo.c
@@ -0,0 +1,62 @@
+#include <jni.h>
+#include <android/log.h>
+#include <stdio.h>
+#include <string.h>
+
+JNIEXPORT jstring JNICALL
+Java_com_imooc_wangyouzhan_chatclient_Activity_NDKActivity_hello(JNIEnv *env, jobject instance) {
+
+    // TODO
+
+
+    return (*env)->NewStringUTF(env, "来之ndk");
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_imooc_wangyouzhan_chatclient_Activity_NDKActivity_getFromJNI(JNIEnv *env,
+                                                                      jobject instance) {
+
+    return (*env)->NewStringUTF(env, "我们的app");
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_imooc_wangyouzhan_chatclient_Activity_NDKActivity_sayHello(JNIEnv *env, jobject instance) {
+
+    // TODO
+
+
+    return (*env)->NewStringUTF(env, "haoma ");
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_imooc_wangyouzhan_chatclient_Activity_NDKActivity_updateFile(JNIEnv *env, jclass type,
+                                                                      jstring file_) {
+    const char *file_path = (*env)->GetStringUTFChars(env, file_, 0);
+
+    if(file_path != NULL){
+        printf("from c file_path %s", file_path);
+    }
+
+    FILE *file = fopen(file_path, "a+");
+    if(file != NULL){
+        __android_log_print(ANDROID_LOG_VERBOSE,"nate","file path %s", file_path);
+    }
+
+    char data[] = "I am a boy";
+    int count = fwrite(data,strlen(data), 1, file);
+    if(count > 0){
+        __android_log_print(ANDROID_LOG_VERBOSE,"nate","form c write file success");
+    }else{
+        __android_log_print(ANDROID_LOG_VERBOSE,"nate","form c write file faile");
+    }
+
+    if(file != NULL){
+        fclose(file);
+    }
+
+
+
+    (*env)->ReleaseStringUTFChars(env, file_, file_path);
+
+    return (*env)->NewStringUTF(env,"成功" );
+}
